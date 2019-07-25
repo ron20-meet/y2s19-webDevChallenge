85ad1b0602c20d72476817d2e2a906ccb7a1c5c7
myname21-meet <myname21@meet.mit.edu>
2019-07-25 11:27:31 -0400

    Ron does not know how to push

--- w3schools.js
+++ w3schools.js
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html>
-<style>
-#myProgress {
-  width: 100%;
-  background-color: #ddd;
-}
 
-#myBar {
-  width: 1%;
-  height: 30px;
-  background-color: #4CAF50;
-}
-</style>
-<body>
-
-<h1>JavaScript Progress Bar</h1>
-
-<div id="myProgress">
-  <div id="myBar"></div>
-</div>
-
-<br>
-<button onclick="move()">Click Me</button> 
-
-<script>
 function move() {
   var elem = document.getElementById("myBar");   
   var width = 1;
@@ -37,7 +12,7 @@ function move() {
     }
   }
 }
-</script>
 
-</body>
-</html>
+$("#goldi").on("click" , function () {
+  alert("hello");
+});
